df389616cabcdadf80d9aefcba4550f7c6d23335
Pavel Dusanek <dusanek@iquest.cz>
2017-01-09 16:35:36 +0100

    AMQP URI parsing

--- lib/logstash-logger/device.rb
+++ lib/logstash-logger/device.rb
@@ -40,26 +40,48 @@ module LogStashLogger
       if uri = opts[:uri]
         require 'uri'
         parsed = ::URI.parse(uri)
-        {type: parsed.scheme, host: parsed.host, port: parsed.port, path: parsed.path}
+        query_params = {}
+        if parsed.query
+          parsed_query = URI.decode_www_form(parsed.query)
+          # symbolize keys
+          query_params = parsed_query.each_with_object({}) {|(k,v),h| h[k.to_sym] = v}
+        end
+        uri_params = case parsed.scheme.to_sym
+                     when :amqp, :amqps
+                       require 'amq/uri'
+                       AMQ::URI.parse(uri).tap do |amq_uri|
+                         amq_uri[:type] = amq_uri.delete(:scheme)
+                         amq_uri[:password] = amq_uri.delete(:pass)
+                       end
+                     else
+                       {
+                         type: parsed.scheme,
+                         host: parsed.host,
+                         port: parsed.port,
+                         path: parsed.path,
+                         user: parsed.user,
+                         password: parsed.password
+                       }
+                     end
+        query_params.merge(uri_params).reject { |_, value| value.nil? }
       end
     end
 
     def self.device_klass_for(type)
       case type.to_sym
-        when :udp then UDP
-        when :tcp then TCP
-        when :unix then Unix
-        when :file then File
-        when :redis then Redis
-        when :kafka then Kafka
-        when :rabbitmq then Rabbitmq
-        when :amqp then Rabbitmq
-        when :io then IO
-        when :stdout then Stdout
-        when :stderr then Stderr
-        when :multi_delegator then MultiDelegator
-        when :balancer then Balancer
-        else fail ArgumentError, 'Invalid device type'
+      when :udp then UDP
+      when :tcp then TCP
+      when :unix then Unix
+      when :file then File
+      when :redis then Redis
+      when :kafka then Kafka
+      when :amqp, :amqps then Rabbitmq
+      when :io then IO
+      when :stdout then Stdout
+      when :stderr then Stderr
+      when :multi_delegator then MultiDelegator
+      when :balancer then Balancer
+      else fail ArgumentError, 'Invalid device type'
       end
     end
   end
